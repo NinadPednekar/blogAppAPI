0dfe30630f2c7b57e6e3204832a6b03a125b7808
Ninad Pednekar <ninadp610@gmail.com>
2023-04-07 20:41:18 +0530

    User API Completed
    Signed-off-by: Ninad Pednekar <ninadp610@gmail.com>

--- src/main/java/com/blog/api/repos/UserRepo.java
+++ src/main/java/com/blog/api/repos/UserRepo.java
@@ -4,6 +4,5 @@ import com.blog.api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
-@Component
 public interface UserRepo extends JpaRepository<User, Integer> {
 }
