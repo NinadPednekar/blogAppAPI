cfb5dd39fb2ef14f9462c86bf00da4b030f04724
Ninad Pednekar <ninadp610@gmail.com>
2023-04-09 15:28:14 +0530

    Added Spring Security
    Signed-off-by: Ninad Pednekar <ninadp610@gmail.com>

---
+++ src/main/java/com/blog/api/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.blog.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+        return http.build();
+    }
+
+    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
+        return null;
+    }
+
+}
